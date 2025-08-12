ee5576f8b9591e9306a4ebfad1693a7b8ccb6a8e
penhele <stephenhelk@gmail.com>
2025-08-13 00:15:21 +0700

    feat: edit pengurus card

--- src/components/PengurusCard.tsx
+++ src/components/PengurusCard.tsx
@@ -4,9 +4,7 @@ import { useEffect, useState } from "react";
 function PengurusCard() {
   type Pengurus = {
     id: number;
-    name: string;
-    position: string;
-    profile_url: string;
+    image_url: string;
   };
 
   const [imageList, setImageList] = useState<Pengurus[]>([]);
@@ -33,16 +31,14 @@ function PengurusCard() {
       <h1 className="titleSection">
         Pengurus Pelkat PT GPIB Maranatha Jakarta Periode 2025-2027
       </h1>
-      <div className="grid grid-cols-2 sm:grid-cols-4 gap-5">
+
+      <div className="flex scrollbar-hide overflow-x-auto snap-x snap-mandatory rounded-lg">
         {imageList.map((item) => (
-          <div key={item.id} className="flex flex-col gap-0.5">
-            <img
-              src={item.profile_url}
-              className="rounded-lg max-w-3xs h-full object-cover"
-            />
-            <p className="bg-amber-100">{item.name}</p>
-            <p className="font-semibold">{item.position}</p>
-          </div>
+          <img
+            key={item.id}
+            src={item.image_url}
+            className="w-full md:w-1/2 aspect-video object-cover snap-center"
+          />
         ))}
       </div>
     </div>
